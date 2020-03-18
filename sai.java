c8af9ed4d712f47ea0bddc6d12f9dab9af1379a4
SaikrishnaTirupathi <62322825+SaikrishnaTirupathi@users.noreply.github.com>
2020-03-18 11:12:30 +0530

    initial commit

---
+++ sai.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sai 
+{
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+	 	Scanner in=new Scanner(System.in);
+		System.out.println("enter the aa anb b values");
+		a=in.nextInt();
+		b=in.nextInt();
+		c=a+b;
+				System.out.println(c);
+
+
+	}
+}
